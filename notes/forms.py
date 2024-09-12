4b84fabb41f54f95f54154ca1e5b8bb25e28d98a
isoldaliborio <isoldaliborio@gmail.com>
2024-09-12 20:03:08 +0100

    delete

--- notes/forms.py
+++ notes/forms.py
@@ -6,9 +6,17 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ("title", "text")
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control my-2"}),
+            "text": forms.Textarea(attrs={"class": "form-control my-2"})
+        }
+        labels = {
+            "text": "Write your notes here"
+
+        }
     
-    def clean_title(self):
-        title = self.cleaned_data["title"]
-        if "Django" not in title:
-            raise ValidationError("We only accept notes about Django!")
-        return title
+    # def clean_title(self):
+    #     title = self.cleaned_data["title"]
+    #     if "Django" not in title:
+    #         raise ValidationError("We only accept notes about Django!")
+    #     return title
